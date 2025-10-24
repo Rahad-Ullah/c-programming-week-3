ee1ddb828b6278ac36bfc38d3fb5ac311d3ed05b
Rahad-Ullah <rahadullah10@gmail.com>
2025-10-24 21:07:14 +0600

    string copy by built-in strcpy func

--- Module-11/strcpy.c
+++ Module-11/strcpy.c
@@ -4,12 +4,7 @@ int main()
     char a[100], b[100];
     scanf("%s %s", &a, &b);
 
-    printf("%s %s\n", a, b);
-
-    for (int i = 0; i <= strlen(b); i++)
-    {
-        a[i] = b[i];
-    }
+    strcpy(a, b);
 
     printf("%s %s", a, b);
 
