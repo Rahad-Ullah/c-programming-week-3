3bded0f078d8c711c5e847e91decb2e5667f817d
Rahad-Ullah <rahadullah10@gmail.com>
2025-10-24 20:56:45 +0600

    string copy

---
+++ Module-11/string_copy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    char a[100], b[100];
+    scanf("%s %s", &a, &b);
+
+    printf("%s %s\n", a, b);
+
+    for (int i = 0; i < strlen(b); i++)
+    {
+        a[i] = b[i];
+    }
+
+    printf("%s %s", a, b);
+
+    return 0;
+}
