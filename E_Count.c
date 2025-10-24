6a8873c69fe77c01a480bb085bb98dbdbf082f9b
Md. Rahad Ullah <rahadullah10@gmail.com>
2025-10-24 12:08:32 +0600

    solve: sum of char digits

---
+++ E_Count.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    char str[1000001];
+    scanf("%s", str);
+
+    int sum = 0;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        sum += str[i] - '0';
+    }
+
+    printf("%d", sum);
+    return 0;
+}
