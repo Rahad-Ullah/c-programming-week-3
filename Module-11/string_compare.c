9b1704a3832a0f4382e2dab7e4823221fdc1e75a
Rahad-Ullah <rahadullah10@gmail.com>
2025-10-24 21:59:40 +0600

    string compare by lexicographical comparison

---
+++ Module-11/string_compare.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    char a[100], b[100];
+    scanf("%s %s", &a, &b);
+
+    // lexicographical comparison
+    int i = 0;
+    while (true)
+    {
+        if (a[i] == '\0' && b[i] == '\0')
+        {
+            printf("Equal");
+            break;
+        }
+        else if (a[i] == '\0')
+        {
+            printf("A is smaller");
+            break;
+        }
+        else if (b[i] == '\0')
+        {
+            printf("B is smaller");
+            break;
+        }
+        else if (a[i] < b[i])
+        {
+            printf("A is smaller");
+            break;
+        }
+        else if (a[i] > b[i])
+        {
+            printf("B is smaller");
+            break;
+        }
+        else
+        {
+            i++;
+        }
+    }
+
+    return 0;
+}
