d32ae17ecf428c08571d10e33a43f3c337e2a6c9
Rahad-Ullah <rahadullah10@gmail.com>
2025-10-31 20:48:17 +0600

    solve: finding lexicographical smallest string

---
+++ Module-11/C_Compare.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str1[21];
+    char str2[21];
+    scanf("%s %s", &str1, &str2);
+
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+    int maxLen = len1 > len2 ? len1 : len2;
+
+    for (int i = 0; i < maxLen; i++)
+    {
+        if (str1[i] == '\0')
+        {
+            printf("%s", str1);
+            break;
+        }
+        else if (str2[i] == '\0')
+        {
+            printf("%s", str2);
+            break;
+        }
+        else if (str1[i] == str2[i])
+        {
+            continue;
+        }
+        else if (str1[i] < str2[i])
+        {
+            printf("%s", str1);
+            break;
+        }
+        else if (str1[i] > str2[i])
+        {
+            printf("%s", str2);
+            break;
+        }
+    }
+
+    return 0;
+}
