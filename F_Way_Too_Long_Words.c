b45219bec30f0770c7bd768692404ef600eb0ec3
Rahad-Ullah <rahadullah10@gmail.com>
2025-10-31 19:29:57 +0600

    solve: problem F

---
+++ F_Way_Too_Long_Words.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++)
+    {
+        char str[101];
+        scanf("%s", str);
+        int length = strlen(str);
+        if (length > 10)
+        {
+            printf("%c%d%c\n", str[0], length - 2, str[length - 1]);
+        }
+        else
+        {
+            printf("%s\n", str);
+        }
+    }
+
+    return 0;
+}
