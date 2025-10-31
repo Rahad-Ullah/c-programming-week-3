0c3984af4a2423d32a436a65b6691205c199e463
Rahad-Ullah <rahadullah10@gmail.com>
2025-10-31 21:09:07 +0600

    fix: comparing lexicographical string runtime error

--- Module-11/C_Compare.c
+++ Module-11/C_Compare.c
@@ -4,13 +4,13 @@ int main()
 {
     char str1[21];
     char str2[21];
-    scanf("%s %s", &str1, &str2);
+    scanf("%s %s", str1, str2);
 
     int len1 = strlen(str1);
     int len2 = strlen(str2);
     int maxLen = len1 > len2 ? len1 : len2;
 
-    for (int i = 0; i < maxLen; i++)
+    for (int i = 0; i <= maxLen; i++)
     {
         if (str1[i] == '\0')
         {
